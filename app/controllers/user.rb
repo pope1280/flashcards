81b8ad9071011ed42b66ddab99b6cb8606ed699b
avanishgiri <dbc@dbc.com>
2013-06-07 14:21:26 -0700

    added current_user helper, added more user routes

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,11 +3,28 @@ get '/login' do
 end
 
 post '/login' do
-  email = params[:user][:email]
-  @user = User.find_by_email(email: email)
-  #need to finish!
+  p params
+  user = User.find_by_email(email: params[:email])
+  
+  if user.password == params[:password]
+    session[:user_id] = user.id
+    #view to render?  profile page maybe?
+  else
+    redirect '/login'
+    #display authentication errors (need to build a partial fro this)
+  end
 end  
 
 get '/sign_up' do
+  p params
+  user = User.create(params[:user])
+  if user.valid?
+    session[:user_id] = user.id
+    #view to render?  profile page maybe?
   erb :'user_views/sign_up'
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
