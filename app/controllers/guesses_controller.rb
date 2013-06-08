e46c8f8cbdf7cbbdacf0bae10aff839605ae9a21
Neil Shah <neilshahteaches@gmail.com>
2013-06-08 16:05:16 -0700

    added messages container in layout, and made a couple of slight changes to guesses and rounds

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -9,12 +9,15 @@ post '/guesses' do
   if @guess.save
     response = @guess.check_answer
     if response[:round_status] == "over"
+      @messages = @guess.result
       erb :'index'
     else
+      debugger
+      @messages = @guess.result
       erb :'/cards/_display_question', :locals => {card: response[:card], deck: @deck, round: @round }
     end
   else
-    @message = @guess.errors.messages
+    @messages = @guess.errors.messages
     erb :'/cards/_display_question', :locals => {card: @card.id, deck: @deck.id, round: @round.id }
   end
 end
