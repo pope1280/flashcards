ebedb8467c1487227d18f77092b88016157f33c8
Neil Shah <neilshahteaches@gmail.com>
2013-06-08 15:09:48 -0700

    working rounds

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,18 @@ class Deck < ActiveRecord::Base
   belongs_to :user
   belongs_to :round
   has_many :cards
+
+
+  def get_next_card(deck, card)
+    card_index = deck.cards.index(card)
+    response = {}
+    if card_index == deck.cards.length - 1
+      response[:round_status] = "over"
+      response[:card] = nil
+    else
+      response[:round_status] = "in_process"
+      response[:card] = deck.cards[card_index + 1]
+    end
+    response
+  end
 end
