05a23779a1731364939111bea4bf054741da0a7b
Neil Shah <neilshahteaches@gmail.com>
2013-06-08 15:09:48 -0700

    working rounds

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -3,13 +3,13 @@ class Guess < ActiveRecord::Base
   belongs_to :card
 
   def check_answer
-    card = Card.find(self.card_id)
-    if card.answer.downcase == self.user_answer.downcase
-      self.result = "right"
-      return true
+    @card = Card.find(self.card_id)
+    @deck = Deck.find(@card.deck_id)
+    if @card.answer == self.user_answer
+      self.result = "correct"
     else
-      self.result = "wrong"
-      return false
+      self.result = "incorrect"
     end
+    @deck.get_next_card(@deck, @card)
   end
 end
